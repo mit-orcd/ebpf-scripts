107f0d113670c5c95d81b29bd43cfc3ee827784e
Luis Turino <turino14@mit.edu>
2026-01-23 14:33:34 -0500

    Log bytes written

--- bpf/traffic-collector.c
+++ bpf/traffic-collector.c
@@ -21,6 +21,11 @@ struct key_t {
     __u32 ipv4;
 };
 
+struct val_t {
+    __u64 requests;
+    __u64 bytes;
+};
+
 // Values of interest (for reference):
 // uid: rqstp->rq_cred.cr_uid.val
 // ip: rqstp->rq_addr
@@ -30,7 +35,7 @@ struct key_t {
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __type(key, struct key_t);
-    __type(value, __u64);
+    __type(value, struct val_t);
     __uint(max_entries, 10240); // 10240 * (8+4+4 + 8) = 10240 * 24 = ~0.25 MB
 } nfs_ops_counts SEC(".maps");
 
@@ -57,15 +62,32 @@ int BPF_PROG(write_ops, struct svc_rqst *rqstp,
 
     // read uid
     key.uid = BPF_CORE_READ(rqstp, rq_cred.cr_uid.val);
-    bpf_printk("nfs write %u\n", key.uid);
 
-    __u64 *count = bpf_map_lookup_elem(&nfs_ops_counts, &key);
-    if (count) {
-        __sync_fetch_and_add(count, 1);
+    // read bytes
+    __u32 bytes = BPF_CORE_READ(u, write.wr_payload.buflen);
+    bpf_printk("nfs write %u\n", bytes);
+
+    struct val_t *val = bpf_map_lookup_elem(&nfs_ops_counts, &key);
+    if (val) {
+        __sync_fetch_and_add(&val->requests, 1);
+        __sync_fetch_and_add(&val->bytes, (__u64)bytes);
         // Note: may instead add bytes written
-        //! u->write.wr_bytes_written
+        // ! u->write.wr_bytes_written
+        /*
+           .wr_bytes_written is how much the server wrote, this can only be
+           accessed as fexit
+
+           .wr_payload contains the information of what the client sent
+           (accessed at fentry)
+             .len
+             .buflen
+        */
+
     } else {
-        __u64 init = 1;
+        struct val_t init = {
+            .requests = 1,
+            .bytes = (__u64)bytes,
+        };
         bpf_map_update_elem(&nfs_ops_counts, &key, &init, BPF_ANY);
     }
 
--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/cilium/ebpf/link"
 )
 
+const atomic = true
+
+const time_per_query = 1
+
 // run `go generate && go build` and then ./nfs-traffic-viewer
 
 func main() {
@@ -37,27 +41,85 @@ func main() {
 
 	log.Printf("Program Loaded!\n")
 
-	ticker := time.NewTicker(5 * time.Second) // tries to do exactly every 5 seconds, regardless of execution time
+	ticker := time.NewTicker(time_per_query * time.Second)
 	defer ticker.Stop()
 
 	// to do: every x seconds - gets all map data (flushes map), adds it to sliding window
-	for range ticker.C {
-
+	for {
 		select {
-			case <-ticker.C:
-				var key collectorKeyT
-				var	val uint64
-				
-				iterator := objs.collectorMaps.NfsOpsCounts.Iterate()
-
-				for iterator.Next(&key, &val) {
-					log.Printf("UID: %d | Inode: %d | Count: %d\n", key.Uid, key.Ino, val)
-				}
-
 			case <-kill_sig:
-				<-kill_sig
 				log.Printf("Exiting...")
 				return
+
+			case <-ticker.C:
+				
+				iterator := objs.collectorMaps.NfsOpsCounts.Iterate()
+				
+
+				if atomic { /* atomic == true */
+					// obtains exact amount of data
+					// first takes a snapshot of current keys
+					// then for each key gathers the values and deletes it from the map
+					var keys []collectorKeyT
+				
+					var valtmp collectorValT
+					var key collectorKeyT
+					for iterator.Next(&key, &valtmp) {
+						keys = append(keys, key)	
+					}
+
+					for _, k := range keys {
+						var val collectorValT
+						if err := objs.collectorMaps.NfsOpsCounts.LookupAndDelete(k, &val); err != nil {
+							log.Printf("Delete error %v", err)
+							continue
+						}
+
+						log.Printf("UID: %d | Inode: %d | Requests: %d | Total Bytes: %d\n", k.Uid, k.Ino, val.Requests, val.Bytes)
+					}
+				} else { /* atomic == false */
+					// much higher throughput, but may lose some data due to race condition
+					// var key collectorKeyT
+					// var val uint64
+					// for iterator.Next(&key, &val) {
+					// 	log.Printf("UID: %d | Inode: %d | Count: %d\n", key.Uid, key.Ino, val)
+					// 	// delete immediately, is this safe?
+					// 	if err := objs.collectorMaps.NfsOpsCounts.Delete(key); err != nil {
+					// 		log.Printf("Delete error %v", err) 
+					// 	}
+						
+					// }
+
+					// todo: make something like this work (much faster but slightly less reliable)
+					// const batchSize = 256
+					// cursor := &ebpf.MapBatchCursor{}
+                    // for {
+                    //     keys := make([]collectorKeyT, batchSize)
+                    //     vals := make([]uint64, batchSize)
+                    //     n, err := objs.collectorMaps.NfsOpsCounts.BatchLookupAndDelete(cursor, keys, vals, nil)
+					// 	log.Printf("%d", n)
+                    //     if err != nil {
+                    //         // treat empty map / no more entries as done
+                    //         if err == ebpf.ErrKeyNotExist || n == 0 {
+                    //             break
+                    //         }
+                    //         log.Printf("Batch error: %v", err)
+                    //         break
+                    //     }
+
+                    //     for i := 0; i < n; i++ {
+                    //         k := keys[i]
+                    //         v := vals[i]
+                    //         log.Printf("UID: %d | Inode: %d | Count: %d\n", k.Uid, k.Ino, v)
+                    //     }
+
+                    //     if n < batchSize {
+                    //         // drained the map
+                    //         break
+                    //     }
+                    // }
+
+				}
 		
 		}		
 	}

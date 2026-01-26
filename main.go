28591d4e1e3acd4dde024d795b4f8eff7b7ba7c2
Luis Turino <turino14@mit.edu>
2026-01-26 12:07:28 -0500

    accumulator logic

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -10,10 +11,7 @@ import (
 )
 
 const atomic = true
-
 const time_per_query = 1
-
-// run `go generate && go build` and then ./nfs-traffic-viewer
 
 func main() {
 
@@ -41,86 +39,81 @@ func main() {
 
 	log.Printf("Program Loaded!\n")
 
+	sw := SlidingWindow{}
+	// sw.time_per_update_ms = 1000
+	// sw.total_display_time_s = 900 // 900s == 15mins
+	sw.total_summary = WindowSummary{}
+	sw.total_summary.m = make(map[uint32]UserMetrics)
+
 	ticker := time.NewTicker(time_per_query * time.Second)
 	defer ticker.Stop()
-
-	// to do: every x seconds - gets all map data (flushes map), adds it to sliding window
 	for {
 		select {
-			case <-kill_sig:
-				log.Printf("Exiting...")
-				return
+		case <-kill_sig:
+			log.Printf("Exiting...")
+			return
 
-			case <-ticker.C:
-				
-				iterator := objs.collectorMaps.NfsOpsCounts.Iterate()
-				
+		case <-ticker.C:
 
-				if atomic { /* atomic == true */
-					// obtains exact amount of data
-					// first takes a snapshot of current keys
-					// then for each key gathers the values and deletes it from the map
-					var keys []collectorKeyT
-				
-					var valtmp collectorValT
-					var key collectorKeyT
-					for iterator.Next(&key, &valtmp) {
-						keys = append(keys, key)	
-					}
+			fmt.Print("\033[H\033[2J")
+			log.Printf("Accumulated Writes:")
 
-					for _, k := range keys {
-						var val collectorValT
-						if err := objs.collectorMaps.NfsOpsCounts.LookupAndDelete(k, &val); err != nil {
-							log.Printf("Delete error %v", err)
-							continue
-						}
-
-						log.Printf("UID: %d | Inode: %d | Requests: %d | Total Bytes: %d\n", k.Uid, k.Ino, val.Requests, val.Bytes)
-					}
-				} else { /* atomic == false */
-					// much higher throughput, but may lose some data due to race condition
-					// var key collectorKeyT
-					// var val uint64
-					// for iterator.Next(&key, &val) {
-					// 	log.Printf("UID: %d | Inode: %d | Count: %d\n", key.Uid, key.Ino, val)
-					// 	// delete immediately, is this safe?
-					// 	if err := objs.collectorMaps.NfsOpsCounts.Delete(key); err != nil {
-					// 		log.Printf("Delete error %v", err) 
-					// 	}
-						
-					// }
-
-					// todo: make something like this work (much faster but slightly less reliable)
-					// const batchSize = 256
-					// cursor := &ebpf.MapBatchCursor{}
-                    // for {
-                    //     keys := make([]collectorKeyT, batchSize)
-                    //     vals := make([]uint64, batchSize)
-                    //     n, err := objs.collectorMaps.NfsOpsCounts.BatchLookupAndDelete(cursor, keys, vals, nil)
-					// 	log.Printf("%d", n)
-                    //     if err != nil {
-                    //         // treat empty map / no more entries as done
-                    //         if err == ebpf.ErrKeyNotExist || n == 0 {
-                    //             break
-                    //         }
-                    //         log.Printf("Batch error: %v", err)
-                    //         break
-                    //     }
-
-                    //     for i := 0; i < n; i++ {
-                    //         k := keys[i]
-                    //         v := vals[i]
-                    //         log.Printf("UID: %d | Inode: %d | Count: %d\n", k.Uid, k.Ino, v)
-                    //     }
-
-                    //     if n < batchSize {
-                    //         // drained the map
-                    //         break
-                    //     }
-                    // }
-
+			for usr, files := range sw.total_summary.m {
+				fmt.Printf("===== UID %d =====\n", usr)
+				for ino, metrics := range files.files {
+					fmt.Printf("ino %d: r%d rb%d w%d wb%d\n", ino, metrics.r_ops_count, metrics.r_bytes, metrics.w_ops_count, metrics.w_bytes)
 				}
-		
-		}		
+			}
+
+			fmt.Printf("\n\n==== LOG ====\n")
+
+			// 1. Switch running map
+			// 2. Create window for current map
+			// 3. Update sliding window
+			// 4. 4
+
+			// todo: move logic into SlidingWindow
+			iterator := objs.collectorMaps.NfsOpsCounts.Iterate()
+
+			// todo: implement two-buffer collections system
+			var keys []collectorKeyT
+
+			var valtmp collectorValT
+			var key collectorKeyT
+			for iterator.Next(&key, &valtmp) {
+				keys = append(keys, key)
+			}
+
+			for _, k := range keys {
+				var val collectorValT
+				if err := objs.collectorMaps.NfsOpsCounts.LookupAndDelete(k, &val); err != nil {
+					log.Printf("Delete error %v", err)
+					continue
+				}
+
+				fmt.Printf("UID: %d | Inode: %d | Requests: %d | Total Bytes: %d\n", k.Uid, k.Ino, val.Requests, val.Bytes)
+
+				um, ok := sw.total_summary.m[k.Uid]
+				if !ok {
+					log.Print("Created new user")
+					um = UserMetrics{
+						files: make(map[uint64]FileMetrics),
+					}
+				}
+				if um.files == nil {
+					um.files = make(map[uint64]FileMetrics)
+				}
+				fm, ok := um.files[k.Ino]
+				if !ok {
+					fm = FileMetrics{}
+				}
+				fm.w_ops_count += val.Requests
+				fm.w_bytes += val.Bytes
+
+				um.files[k.Ino] = fm
+				sw.total_summary.m[k.Uid] = um
+			}
+
+		}
 	}
 }
